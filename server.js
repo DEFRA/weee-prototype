c83b1245d116b504c415e39e576443c9f205d4ad
sophieottaway@yahoo.co.uk <sophieottaway@yahoo.co.uk>
2016-02-16 09:05:25 +0000

    commit

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.set('views', path.join(__dirname, '/app/views'));
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_template/assets'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit'));
+app.use('/public', express.static(__dirname + '/app/assets/csv'));
 app.use('/public/images/icons', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit/images'));
 // Elements refers to icon folder instead of images folder
 
