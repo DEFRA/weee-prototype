52fb65196d2dcc698b1d76ae96f76cbece92cc57
SamGoodeySFW <SamuelG@sfwltd.co.uk>
2018-09-18 12:59:25 +0100

    Resolving a few bugs that were picked up

--- app/routes.js
+++ app/routes.js
@@ -33,23 +33,38 @@ router.post('/AATF-Returns/save-and-continue', function (req, res) {
 
 router.post('/AATF-Returns/save-and-continue-change', function (req, res) {
     var items = [req.session.data['large-household-appliances-input-SC009'], req.session.data['small-household-appliances-input-SC009'], req.session.data['it-and-telecomms-input-SC009'], req.session.data['consumer-equipment-input-SC009'], req.session.data['lighting-equipment-input-SC009'], req.session.data['electrical-and-electronic-input-SC009'], req.session.data['toys-leisure-sports-input-SC009'], req.session.data['medical-devices-input-SC009'], req.session.data['monitoring-control-input-SC009'], req.session.data['automatic-dispensers-input-SC009'], req.session.data['display-equipment-input-SC009'], req.session.data['cooling-appliance-input-SC009'], req.session.data['gas-discharge-led-input-SC009'], req.session.data['photovolatic-panels-input-SC009']]
+    var itemsB2C = [req.session.data['large-household-appliances-input-SC009-b2c'], req.session.data['small-household-appliances-input-SC009-b2c'], req.session.data['it-and-telecomms-input-SC009-b2c'], req.session.data['consumer-equipment-input-SC009-b2c'], req.session.data['lighting-equipment-input-SC009-b2c'], req.session.data['electrical-and-electronic-input-SC009-b2c'], req.session.data['toys-leisure-sports-input-SC009-b2c'], req.session.data['medical-devices-input-SC009-b2c'], req.session.data['monitoring-control-input-SC009-b2c'], req.session.data['automatic-dispensers-input-SC009-b2c'], req.session.data['display-equipment-input-SC009-b2c'], req.session.data['cooling-appliance-input-SC009-b2c'], req.session.data['gas-discharge-led-input-SC009-b2c'], req.session.data['photovolatic-panels-input-SC009-b2c']]
+
     var result = 0;
     for (var i = 0; i < items.length; i++) {
         result += Number(items[i])
+    }
+    for (var i = 0; i < itemsB2C.length; i++) {
+        result += Number(itemsB2C[i])
     }
     req.session.data['WEEE-received-for-treatment-result'] = result.toFixed(3);
 
     var items = [req.session.data['large-household-appliances-input-SC010'], req.session.data['small-household-appliances-input-SC010'], req.session.data['it-and-telecomms-input-SC010'], req.session.data['consumer-equipment-input-SC010'], req.session.data['lighting-equipment-input-SC010'], req.session.data['electrical-and-electronic-input-SC010'], req.session.data['toys-leisure-sports-input-SC010'], req.session.data['medical-devices-input-SC010'], req.session.data['monitoring-control-input-SC010'], req.session.data['automatic-dispensers-input-SC010'], req.session.data['display-equipment-input-SC010'], req.session.data['cooling-appliance-input-SC010'], req.session.data['gas-discharge-led-input-SC010'], req.session.data['photovolatic-panels-input-SC010']]
+    var itemsB2C = [req.session.data['large-household-appliances-input-SC010-b2c'], req.session.data['small-household-appliances-input-SC010-b2c'], req.session.data['it-and-telecomms-input-SC010-b2c'], req.session.data['consumer-equipment-input-SC010-b2c'], req.session.data['lighting-equipment-input-SC010-b2c'], req.session.data['electrical-and-electronic-input-SC010-b2c'], req.session.data['toys-leisure-sports-input-SC010-b2c'], req.session.data['medical-devices-input-SC010-b2c'], req.session.data['monitoring-control-input-SC010-b2c'], req.session.data['automatic-dispensers-input-SC010-b2c'], req.session.data['display-equipment-input-SC010-b2c'], req.session.data['cooling-appliance-input-SC010-b2c'], req.session.data['gas-discharge-led-input-SC010-b2c'], req.session.data['photovolatic-panels-input-SC010-b2c']]
+
     var result = 0;
     for (var i = 0; i < items.length; i++) {
         result += Number(items[i])
     }
+    for (var i = 0; i < itemsB2C.length; i++) {
+        result += Number(itemsB2C[i])
+    }
     req.session.data['WEEE-resused-as-a-whole-appliance-result'] = result.toFixed(3)
 
     var items = [req.session.data['large-household-appliances-input-SC011'], req.session.data['small-household-appliances-input-SC011'], req.session.data['it-and-telecomms-input-SC011'], req.session.data['consumer-equipment-input-SC011'], req.session.data['lighting-equipment-input-SC011'], req.session.data['electrical-and-electronic-input-SC011'], req.session.data['toys-leisure-sports-input-SC011'], req.session.data['medical-devices-input-SC011'], req.session.data['monitoring-control-input-SC011'], req.session.data['automatic-dispensers-input-SC011'], req.session.data['display-equipment-input-SC011'], req.session.data['cooling-appliance-input-SC011'], req.session.data['gas-discharge-led-input-SC011'], req.session.data['photovolatic-panels-input-SC011']]
+    var itemsB2C = [req.session.data['large-household-appliances-input-SC011-b2c'], req.session.data['small-household-appliances-input-SC011-b2c'], req.session.data['it-and-telecomms-input-SC011-b2c'], req.session.data['consumer-equipment-input-SC011-b2c'], req.session.data['lighting-equipment-input-SC011-b2c'], req.session.data['electrical-and-electronic-input-SC011-b2c'], req.session.data['toys-leisure-sports-input-SC011-b2c'], req.session.data['medical-devices-input-SC011-b2c'], req.session.data['monitoring-control-input-SC011-b2c'], req.session.data['automatic-dispensers-input-SC011-b2c'], req.session.data['display-equipment-input-SC011-b2c'], req.session.data['cooling-appliance-input-SC011-b2c'], req.session.data['gas-discharge-led-input-SC011-b2c'], req.session.data['photovolatic-panels-input-SC011-b2c']]
+
     var result = 0;
     for (var i = 0; i < items.length; i++) {
         result += Number(items[i])
+    }
+    for (var i = 0; i < itemsB2C.length; i++) {
+        result += Number(itemsB2C[i])
     }
     req.session.data['Whole-WEEE-sent-to-another-treatment-result'] = result.toFixed(3)
     
@@ -66,23 +81,35 @@ router.post('/AATF-Returns/facility-save', function (req, res) {
 
 router.post('/AATF-Returns/facility-save-change', function (req, res) {
     var items = [req.session.data['large-household-appliances-input-SC009'], req.session.data['small-household-appliances-input-SC009'], req.session.data['it-and-telecomms-input-SC009'], req.session.data['consumer-equipment-input-SC009'], req.session.data['lighting-equipment-input-SC009'], req.session.data['electrical-and-electronic-input-SC009'], req.session.data['toys-leisure-sports-input-SC009'], req.session.data['medical-devices-input-SC009'], req.session.data['monitoring-control-input-SC009'], req.session.data['automatic-dispensers-input-SC009'], req.session.data['display-equipment-input-SC009'], req.session.data['cooling-appliance-input-SC009'], req.session.data['gas-discharge-led-input-SC009'], req.session.data['photovolatic-panels-input-SC009']]
+    var itemsB2C = [req.session.data['large-household-appliances-input-SC009-b2c'], req.session.data['small-household-appliances-input-SC009-b2c'], req.session.data['it-and-telecomms-input-SC009-b2c'], req.session.data['consumer-equipment-input-SC009-b2c'], req.session.data['lighting-equipment-input-SC009-b2c'], req.session.data['electrical-and-electronic-input-SC009-b2c'], req.session.data['toys-leisure-sports-input-SC009-b2c'], req.session.data['medical-devices-input-SC009-b2c'], req.session.data['monitoring-control-input-SC009-b2c'], req.session.data['automatic-dispensers-input-SC009-b2c'], req.session.data['display-equipment-input-SC009-b2c'], req.session.data['cooling-appliance-input-SC009-b2c'], req.session.data['gas-discharge-led-input-SC009-b2c'], req.session.data['photovolatic-panels-input-SC009-b2c']]
     var result = 0;
     for (var i = 0; i < items.length; i++) {
         result += Number(items[i])
+    }
+    for (var i = 0; i < itemsB2C.length; i++) {
+        result += Number(itemsB2C[i])
     }
     req.session.data['WEEE-received-for-treatment-result'] = result.toFixed(3);
 
     var items = [req.session.data['large-household-appliances-input-SC010'], req.session.data['small-household-appliances-input-SC010'], req.session.data['it-and-telecomms-input-SC010'], req.session.data['consumer-equipment-input-SC010'], req.session.data['lighting-equipment-input-SC010'], req.session.data['electrical-and-electronic-input-SC010'], req.session.data['toys-leisure-sports-input-SC010'], req.session.data['medical-devices-input-SC010'], req.session.data['monitoring-control-input-SC010'], req.session.data['automatic-dispensers-input-SC010'], req.session.data['display-equipment-input-SC010'], req.session.data['cooling-appliance-input-SC010'], req.session.data['gas-discharge-led-input-SC010'], req.session.data['photovolatic-panels-input-SC010']]
+    var itemsB2C = [req.session.data['large-household-appliances-input-SC010-b2c'], req.session.data['small-household-appliances-input-SC010-b2c'], req.session.data['it-and-telecomms-input-SC010-b2c'], req.session.data['consumer-equipment-input-SC010-b2c'], req.session.data['lighting-equipment-input-SC010-b2c'], req.session.data['electrical-and-electronic-input-SC010-b2c'], req.session.data['toys-leisure-sports-input-SC010-b2c'], req.session.data['medical-devices-input-SC010-b2c'], req.session.data['monitoring-control-input-SC010-b2c'], req.session.data['automatic-dispensers-input-SC010-b2c'], req.session.data['display-equipment-input-SC010-b2c'], req.session.data['cooling-appliance-input-SC010-b2c'], req.session.data['gas-discharge-led-input-SC010-b2c'], req.session.data['photovolatic-panels-input-SC010-b2c']]
     var result = 0;
     for (var i = 0; i < items.length; i++) {
         result += Number(items[i])
     }
+    for (var i = 0; i < itemsB2C.length; i++) {
+        result += Number(itemsB2C[i])
+    }
     req.session.data['WEEE-resused-as-a-whole-appliance-result'] = result.toFixed(3)
 
     var items = [req.session.data['large-household-appliances-input-SC011'], req.session.data['small-household-appliances-input-SC011'], req.session.data['it-and-telecomms-input-SC011'], req.session.data['consumer-equipment-input-SC011'], req.session.data['lighting-equipment-input-SC011'], req.session.data['electrical-and-electronic-input-SC011'], req.session.data['toys-leisure-sports-input-SC011'], req.session.data['medical-devices-input-SC011'], req.session.data['monitoring-control-input-SC011'], req.session.data['automatic-dispensers-input-SC011'], req.session.data['display-equipment-input-SC011'], req.session.data['cooling-appliance-input-SC011'], req.session.data['gas-discharge-led-input-SC011'], req.session.data['photovolatic-panels-input-SC011']]
+    var itemsB2C = [req.session.data['large-household-appliances-input-SC011-b2c'], req.session.data['small-household-appliances-input-SC011-b2c'], req.session.data['it-and-telecomms-input-SC011-b2c'], req.session.data['consumer-equipment-input-SC011-b2c'], req.session.data['lighting-equipment-input-SC011-b2c'], req.session.data['electrical-and-electronic-input-SC011-b2c'], req.session.data['toys-leisure-sports-input-SC011-b2c'], req.session.data['medical-devices-input-SC011-b2c'], req.session.data['monitoring-control-input-SC011-b2c'], req.session.data['automatic-dispensers-input-SC011-b2c'], req.session.data['display-equipment-input-SC011-b2c'], req.session.data['cooling-appliance-input-SC011-b2c'], req.session.data['gas-discharge-led-input-SC011-b2c'], req.session.data['photovolatic-panels-input-SC011-b2c']]
     var result = 0;
     for (var i = 0; i < items.length; i++) {
         result += Number(items[i])
+    }
+    for (var i = 0; i < itemsB2C.length; i++) {
+        result += Number(itemsB2C[i])
     }
     req.session.data['Whole-WEEE-sent-to-another-treatment-result'] = result.toFixed(3)
 
@@ -106,19 +133,27 @@ router.post('/AATF-Returns/scheme-add', function (req, res) {
 
 router.post('/AATF-Returns/weee-received-for-treatment-save', function (req, res) {
     var items = [req.session.data['large-household-appliances-input-SC009'], req.session.data['small-household-appliances-input-SC009'], req.session.data['it-and-telecomms-input-SC009'], req.session.data['consumer-equipment-input-SC009'], req.session.data['lighting-equipment-input-SC009'], req.session.data['electrical-and-electronic-input-SC009'], req.session.data['toys-leisure-sports-input-SC009'], req.session.data['medical-devices-input-SC009'], req.session.data['monitoring-control-input-SC009'], req.session.data['automatic-dispensers-input-SC009'], req.session.data['display-equipment-input-SC009'], req.session.data['cooling-appliance-input-SC009'], req.session.data['gas-discharge-led-input-SC009'], req.session.data['photovolatic-panels-input-SC009']]
+    var itemsB2C = [req.session.data['large-household-appliances-input-SC009-b2c'], req.session.data['small-household-appliances-input-SC009-b2c'], req.session.data['it-and-telecomms-input-SC009-b2c'], req.session.data['consumer-equipment-input-SC009-b2c'], req.session.data['lighting-equipment-input-SC009-b2c'], req.session.data['electrical-and-electronic-input-SC009-b2c'], req.session.data['toys-leisure-sports-input-SC009-b2c'], req.session.data['medical-devices-input-SC009-b2c'], req.session.data['monitoring-control-input-SC009-b2c'], req.session.data['automatic-dispensers-input-SC009-b2c'], req.session.data['display-equipment-input-SC009-b2c'], req.session.data['cooling-appliance-input-SC009-b2c'], req.session.data['gas-discharge-led-input-SC009-b2c'], req.session.data['photovolatic-panels-input-SC009-b2c']]
     var result = 0;
     for (var i = 0; i < items.length; i++) {
         result += Number(items[i])
+    }
+    for (var i = 0; i < itemsB2C.length; i++) {
+        result += Number(itemsB2C[i])
     }
     req.session.data['WEEE-received-for-treatment-result'] = result.toFixed(3);
     res.redirect('/AATF-Returns/SC007-AATF-Tasklist')
 })
 
 router.post('/AATF-Returns/weee-reused-as-a-whole-appliance-save', function (req, res) {
+    var itemsB2C = [req.session.data['large-household-appliances-input-SC010-b2c'], req.session.data['small-household-appliances-input-SC010-b2c'], req.session.data['it-and-telecomms-input-SC010-b2c'], req.session.data['consumer-equipment-input-SC010-b2c'], req.session.data['lighting-equipment-input-SC010-b2c'], req.session.data['electrical-and-electronic-input-SC010-b2c'], req.session.data['toys-leisure-sports-input-SC010-b2c'], req.session.data['medical-devices-input-SC010-b2c'], req.session.data['monitoring-control-input-SC010-b2c'], req.session.data['automatic-dispensers-input-SC010-b2c'], req.session.data['display-equipment-input-SC010-b2c'], req.session.data['cooling-appliance-input-SC010-b2c'], req.session.data['gas-discharge-led-input-SC010-b2c'], req.session.data['photovolatic-panels-input-SC010-b2c']]
     var items = [req.session.data['large-household-appliances-input-SC010'], req.session.data['small-household-appliances-input-SC010'], req.session.data['it-and-telecomms-input-SC010'], req.session.data['consumer-equipment-input-SC010'], req.session.data['lighting-equipment-input-SC010'], req.session.data['electrical-and-electronic-input-SC010'], req.session.data['toys-leisure-sports-input-SC010'], req.session.data['medical-devices-input-SC010'], req.session.data['monitoring-control-input-SC010'], req.session.data['automatic-dispensers-input-SC010'], req.session.data['display-equipment-input-SC010'], req.session.data['cooling-appliance-input-SC010'], req.session.data['gas-discharge-led-input-SC010'], req.session.data['photovolatic-panels-input-SC010']]
     var result = 0;
     for (var i = 0; i < items.length; i++) {
         result += Number(items[i])
+    }
+    for (var i = 0; i < itemsB2C.length; i++) {
+        result += Number(itemsB2C[i])
     }
     req.session.data['WEEE-resused-as-a-whole-appliance-result'] = result.toFixed(3)
     res.redirect('/AATF-Returns/SC007-AATF-Tasklist')
@@ -126,9 +161,13 @@ router.post('/AATF-Returns/weee-reused-as-a-whole-appliance-save', function (req
 
 router.post('/AATF-Returns/Whole-WEEE-sent-to-another-treatment-save', function (req, res) {
     var items = [req.session.data['large-household-appliances-input-SC011'], req.session.data['small-household-appliances-input-SC011'], req.session.data['it-and-telecomms-input-SC011'], req.session.data['consumer-equipment-input-SC011'], req.session.data['lighting-equipment-input-SC011'], req.session.data['electrical-and-electronic-input-SC011'], req.session.data['toys-leisure-sports-input-SC011'], req.session.data['medical-devices-input-SC011'], req.session.data['monitoring-control-input-SC011'], req.session.data['automatic-dispensers-input-SC011'], req.session.data['display-equipment-input-SC011'], req.session.data['cooling-appliance-input-SC011'], req.session.data['gas-discharge-led-input-SC011'], req.session.data['photovolatic-panels-input-SC011']]
+    var itemsB2C = [req.session.data['large-household-appliances-input-SC011-b2c'], req.session.data['small-household-appliances-input-SC011-b2c'], req.session.data['it-and-telecomms-input-SC011-b2c'], req.session.data['consumer-equipment-input-SC011-b2c'], req.session.data['lighting-equipment-input-SC011-b2c'], req.session.data['electrical-and-electronic-input-SC011-b2c'], req.session.data['toys-leisure-sports-input-SC011-b2c'], req.session.data['medical-devices-input-SC011-b2c'], req.session.data['monitoring-control-input-SC011-b2c'], req.session.data['automatic-dispensers-input-SC011-b2c'], req.session.data['display-equipment-input-SC011-b2c'], req.session.data['cooling-appliance-input-SC011-b2c'], req.session.data['gas-discharge-led-input-SC011-b2c'], req.session.data['photovolatic-panels-input-SC011-b2c']]
     var result = 0;
     for (var i = 0; i < items.length; i++) {
         result += Number(items[i])
+    }
+    for (var i = 0; i < itemsB2C.length; i++) {
+        result += Number(itemsB2C[i])
     }
     req.session.data['Whole-WEEE-sent-to-another-treatment-result'] = result.toFixed(3)
     res.redirect('/AATF-Returns/SC007-AATF-Tasklist')
