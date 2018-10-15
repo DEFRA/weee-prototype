63eb3a22a1e62402ee297a4d6ed5ef4068648204
JohnPadgettCivica <JohnPadgettCivica>
2018-10-15 11:18:14 +0100

    totals on copy and paste

--- app/assets/javascripts/transform-paste.js
+++ app/assets/javascripts/transform-paste.js
@@ -23,35 +23,56 @@
             setTimeout(function () {
                 console.log('doing stuff')
                 var data = pasteArea[0].value;
-                var rowsData = data.split('\n');
-                // check the rows and cells
-                //var rowsDataCount = rowsData.length - 1;
-                var coloumnCount = 0;
-                console.log(rowsData);
-                if (rowsData.length > 0) {
-                    console.log('column count' + coloumnCount);
-                    var enterDataTable = document.querySelectorAll('.govuk-table')[0];
-                    // rows
-                    var tableRows = enterDataTable.children[0].children;
-                    if (tableRows && tableRows.length > 1){
-                        var tableRowColumns = tableRows[0].cells.length;
-                        for (var tableRowCount = 1; tableRowCount < tableRows.length - 1; tableRowCount++) {
-                            for (var tableColumnCount = 1; tableColumnCount <= tableRowColumns -1; tableColumnCount++){
-                                var copyValue = rowsData[tableRowCount-1];
-                                if (copyValue){
-                                    var cellData = copyValue.split('\t');
-                                    var cellValue = cellData[tableColumnCount -1];
-                                    console.log(cellData)
-                                    var input = tableRows[tableRowCount].cells[tableColumnCount].querySelector('input:first-of-type');
-                                    if (!isNaN(cellValue)){
-                                        input.value = parseInt(cellValue).toFixed(3);
+                if (data){
+                    var rowsData = data.split('\n');
+                    // check the rows and cells
+                    //var rowsDataCount = rowsData.length - 1;
+                    var coloumnCount = 0;
+                    console.log(rowsData);
+                    if (rowsData.length > 0) {
+                        console.log('column count' + coloumnCount);
+                        var enterDataTable = document.querySelectorAll('.govuk-table')[0];
+                        // rows
+                        var tableRows = enterDataTable.children[0].children;
+                        if (tableRows && tableRows.length > 1){
+                            var tableRowColumns = tableRows[0].cells.length;
+                            for (var tableRowCount = 1; tableRowCount < tableRows.length - 1; tableRowCount++) {
+                                
+                                for (var tableColumnCount = 1; tableColumnCount <= tableRowColumns -1; tableColumnCount++){
+                                    var copyValue = rowsData[tableRowCount-1];
+                                    if (copyValue){
+                                        console.log('copy value' + copyValue)
+                                        var cellData = copyValue.split('\t');
+                                        var cellValue = cellData[tableColumnCount -1];
+                                        var input = tableRows[tableRowCount].cells[tableColumnCount].querySelector('input:first-of-type');
+                                        if (!isNaN(cellValue)){
+                                            var convertedValue = parseFloat(cellValue).toFixed(3);
+                                            input.value = convertedValue;
+                                           
+                                        }
                                     }
                                 }
+                            }
+
+                            // totals
+                            for (var tableColumnCount = 1; tableColumnCount <= tableRowColumns -1; tableColumnCount++){
+                                var columnTotal = 0;
+                                for (var tableRowCount = 1; tableRowCount < tableRows.length - 1; tableRowCount++) {
+                                    var input = tableRows[tableRowCount].cells[tableColumnCount].querySelector('input:first-of-type');
+                                    if (!isNaN(input.value)){
+                                        columnTotal += parseFloat(input.value);
+                                    }
+                                }
+                                var totalControl = tableRows[tableRowCount].cells[tableColumnCount].querySelector('[id*="tonne"]');
+                                totalControl.innerText = columnTotal.toFixed(3);
+                                console.log(columnTotal);
+                                
 
                             }
                         }
                     }
                 }
+                
 
             }, 1000);
 
