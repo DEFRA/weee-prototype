33407e4fb7067417114d5291c3fca227a582762b
JohnPadgettCivica <JohnPadgettCivica>
2018-11-15 16:37:46 +0000

    condition display of aatf return rows

--- app/routes/version1-8/routes.js
+++ app/routes/version1-8/routes.js
@@ -259,7 +259,7 @@ router.get('/version1-8/AATF-Returns/Are-you-sending-any-WEEE-to-another-ATF-for
 })
 */
 router.get('/version1-8/AATF-Returns/SC004-Would-you-like-to-report-on-any-non-obligated-weee', function (req, res) {
-    res.redirect('/version1-8/AATF-Returns//What-do-you-need-to-report-on');
+    res.redirect('/version1-8/AATF-Returns/What-do-you-need-to-report-on');
 })
 
 router.get('/version1-8/AATF-Returns/aatf-option-select', function (req, res) {
