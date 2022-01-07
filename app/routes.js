f60a55910465464eeb40d1b0d2ee178f675dc7c3
John Padgett <John.Padgett@civica.co.uk>
2022-01-07 14:52:04 +0000

    add in initial versions for settle centre

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const version1 = require('./routes/version-1/routes')
 // Add your routes here - above the module.exports line
+
+router.get('/', function (req, res) {
+    res.redirect('/version-1/index')
+})
+
+//Index of versions route
+router.get('/home', function (req, res) {
+    res.redirect('/versions-home')
+})
+
+router.use(version1);
 
 module.exports = router
