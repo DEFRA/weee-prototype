474d0e3e06411776fe6a45b21532068718ba1745
John Padgett <John.Padgett@civica.co.uk>
2022-01-21 15:35:52 +0000

    Add copy and paste to new screens

--- app/routes/version-2/routes.js
+++ app/routes/version-2/routes.js
@@ -1,19 +1,27 @@
 const router = require('express').Router();
 
-
-
-router.post('/create-new-evidence-redirect', function (req, res){
-    res.redirect('/version-2/205_Create_evidence_note_no_protocol');
-});
-
-router.post('/manage-evidence-redirect', function (req, res) {
-    
+router.post('/version-2/manage-evidence-redirect', function (req, res) {
+    req.session.data['paste-values'] = '';
     const activity = req.session.data['choose-activity'];
     console.log(activity)
     if (activity === '7') {
         res.redirect('/version-2/202_Choose_site');
     }
     res.redirect('/version-2/index');
+});
+
+router.post('/version-2/paste-values-save', function (req, res) {
+    req.session.data['paste-values'] = req.session.data['paste-text-area'];
+    res.redirect('/version-2/' + req.session.data['paste-return-page']);
+});
+
+router.get('/version-2/copy-paste-redirect', function(req, res){
+    req.session.data['paste-return-page'] = req.query['returnUrl'];
+    res.redirect('/version-2/copy-paste');
+});
+
+router.post('/version-2/cancel-copy-paste-redirect', function (req, res) {
+    res.redirect('/version-2/' + req.session.data['paste-return-page']);
 });
 
 module.exports = router;
