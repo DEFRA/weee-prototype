f64692c409a8b5cd1cc383402a38fc481d1fccdd
GuidoCangelosi <guido.cangelosi@gmail.com>
2022-02-01 10:44:53 +0000

    fixed the bug when using some filters not working

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,16 +1,24 @@
 
     var table = document.getElementById("evidence-table");
 
-    if (table) {
-        document.getElementById("pcs-filter").addEventListener('change', (e) => {
-            filter();
+    if (table) 
+	{
+        document.getElementById("input-search").addEventListener('change', (e) => {
+            search();        
         });
-    
+	
+        document.getElementById("year-filter").addEventListener('change', (e) => {
+            filter();        
+        });
+		
         document.getElementById("date-filter").addEventListener('change', (e) => {
             filter();
         });
-    
-    
+		
+        document.getElementById("pcs-filter").addEventListener('change', (e) => {
+            filter();
+        });
+		
         document.getElementById("status-filter").addEventListener('change', (e) => {
             filter();
         });
@@ -18,32 +26,25 @@
         document.getElementById("type-filter").addEventListener('change', (e) => {
             filter();
         });
-    
-        document.getElementById("year-filter").addEventListener('change', (e) => {
-            filter();        
-        });
-
-        document.getElementById("input-search").addEventListener('change', (e) => {
-            search();        
-        });
-        
     }
     
 
-    function IsMatch(td, compare){
-        if (td) {
-            txtValue = td.textContent || td.innerText;
+    function IsMatch(td, compare)
+	{
+        if (td) 
+		{
+            var txtValue = td.textContent || td.innerText;
             console.log('content ' + txtValue + ' compare ' + compare);
-            if (txtValue.indexOf(compare) === -1) {
+            if (txtValue.indexOf(compare) === -1) 
+			{
                 return false;
             }
         }
         return true;
     }
 
-    function search() {
-        // Declare variables
-        var input, filter, table, tr, td, i, txtValue;
+    function search() 
+	{
         var searchFilter = document.getElementById("input-search").value;
         var table = document.getElementById("evidence-table");
         var tr = table.getElementsByClassName("govuk-table__row result-row");
@@ -53,88 +54,107 @@
             var displayRow = true;
 
             if (searchFilter.trim() != '' && searchFilter != '') {
-                var td = tr[i].getElementsByTagName("td")[2];
+                var td = tr[i].getElementsByTagName("td")[0];
                 
                 if (!IsMatch(td, searchFilter)){
                     displayRow = false;
                 }
             }
 
-
             if (displayRow){
                 tr[i].style.display = "";
             }
             else{
                 tr[i].style.display = "none";
             }
-            //tr[i].style.display = "";
-            //tr[i].style.display = "none";
         }
       }
 
-    function filter() {
-        // Declare variables
-        var input, filter, table, tr, td, i, txtValue;
+    function filter() 
+	{
+        var table = document.getElementById("evidence-table");
+        var tr = table.getElementsByClassName("govuk-table__row result-row");
+
         var pcsFilter = document.getElementById("pcs-filter").value;
         var yearFilter = document.getElementById("year-filter").value;
         var typeFilter = document.getElementById("type-filter").value;
         var statusFilter = document.getElementById("status-filter").value;
-        var dateFilter = document.getElementById("date-filter").value;
-        var table = document.getElementById("evidence-table");
-        var tr = table.getElementsByClassName("govuk-table__row result-row");
-      
+		
+		// change format to dd/MM/yyyy
+		var dateFilter = document.getElementById("date-filter").value;
+		if ( dateFilter != '' )
+		{
+			var parsedDate = new Date(dateFilter);
+			var thisDate = String(parsedDate.getDate()).padStart(2, '0') 
+							 + '/' 
+							 + (String(parsedDate.getMonth() + 1)).padStart(2, '0') 
+							 + '/' 
+							 + parsedDate.getFullYear();
+			dateFilter = thisDate;
+		}
+
         // Loop through all table rows, and hide those who don't match the search query
-        for (i = 0; i < tr.length; i++) {
+        for (i = 0; i < tr.length; i++) 
+		{
             var displayRow = true;
 
-        
-            if (yearFilter != 0) {
-                var td = tr[i].getElementsByTagName("td")[0];
-                
-                if (!IsMatch(td, yearFilter)){
-                    displayRow = false;
-                }
-            }
-
-            if (dateFilter.trim() != '' && dateFilter != '') {
+            if (yearFilter != 'Compliance year') 
+			{
                 var td = tr[i].getElementsByTagName("td")[1];
                 
-                if (!IsMatch(td, dateFilter)){
+                if (!IsMatch(td, yearFilter))
+				{
                     displayRow = false;
                 }
             }
 
-            if (typeFilter != 0) {
-                var td = tr[i].getElementsByTagName("td")[5];
+            if (dateFilter.trim() != '' && dateFilter != '') 
+			{
+                var td = tr[i].getElementsByTagName("td")[1];
                 
-                if (!IsMatch(td, typeFilter)){
+                if (!IsMatch(td, dateFilter))
+				{
                     displayRow = false;
                 }
             }
 
-            if (pcsFilter.trim() != '' && pcsFilter != 'Issued to') {
+            if (pcsFilter.trim() != '' && pcsFilter != 'Issued to' && pcsFilter != 'Issued by') 
+			{
+                var td = tr[i].getElementsByTagName("td")[2];
+                
+                if (!IsMatch(td, pcsFilter))
+				{
+                    displayRow = false;
+                }
+            }
+
+            if (typeFilter != 'Type') 
+			{
+                var td = tr[i].getElementsByTagName("td")[3];
+                
+                if (!IsMatch(td, typeFilter))
+				{
+                    displayRow = false;
+                }
+            }
+
+            if (statusFilter != 'Status') 
+			{
                 var td = tr[i].getElementsByTagName("td")[4];
-                
-                if (!IsMatch(td, pcsFilter)){
+
+                if (!IsMatch(td, statusFilter))
+				{
                     displayRow = false;
                 }
             }
 
-            if (statusFilter != 0) {
-                var td = tr[i].getElementsByTagName("td")[6];
-
-                if (!IsMatch(td, statusFilter)){
-                    displayRow = false;
-                }
-            }
-
-            if (displayRow){
+            if (displayRow)
+			{
                 tr[i].style.display = "";
             }
-            else{
+            else
+			{
                 tr[i].style.display = "none";
             }
-            //tr[i].style.display = "";
-            //tr[i].style.display = "none";
         }
       }
